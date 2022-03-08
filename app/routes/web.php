5916134e0572b00e26dc2dd7a4eb3db0720f25dc
ryo-ebata <r.ebata@hajimari.inc>
2022-03-08 11:20:33 +0900

    クロージャールーティングの設定をしました

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,9 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['middleware', 'auth'])->group( function(){
+
+    Route::resource('users', UsersController::class);
+
+});
