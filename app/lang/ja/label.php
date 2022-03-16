29d5c820098840e3478eb9927f43d26a9f6d7405
ryo-ebata <r.ebata@hajimari.inc>
2022-03-16 13:15:18 +0900

    日本語化

--- app/lang/ja/label.php
+++ app/lang/ja/label.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'Name'                  => 'お名前',
     'Email'                 => 'メールアドレス',
     'Password'              => 'パスワード',
     'Confirm'               => 'パスワード確認'
